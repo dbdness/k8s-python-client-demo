b9a2b531b6011e19aea0f1a935f8b171870ab52f
Danny Nielsen <dni@delegate.dk>
2018-12-04 15:28:23 +0100

    Add script for on-demand Job creation

--- sample/create_job.py
+++ sample/create_job.py
@@ -1,22 +1,22 @@
-"""On-demand Kubernetes Deployment creation
+"""On-demand Kubernetes Job creation
 
-This script creates a Deployment within the default Kubernetes cluster context.
+This script creates a Job within the default Kubernetes cluster context.
 
 This script also requires that `kubernetes` is installed within the Python
 environment you are running this script in.
 
 Variables:
-    deployment_name (str): The desired deployment name
+    job_name (str): The desired Job name
     replicas (int): Number of replicas
     pod_labels (dict{str:str}): Labels to assign to the Pod
     container_name (str): The desired name of the container
     container_image (str): The full path to the desired container image. Repository has to be included.
-    container_port (int): The exposed port from within the Deployment.
+    container_port (int): The exposed port from within the Job.
 """
 
 from kubernetes import client, config
 
-deployment_name = "demo"
+job_name = "demo"
 replicas = 1
 pod_labels = {"app": "sentence-reverser"}
 container_name = "sentence-reverser"
@@ -32,20 +32,18 @@ container_port = 80
 
 # Loading default config
 config.load_kube_config()
-extension = client.ExtensionsV1beta1Api()
+extension = client.BatchV1Api()
 
-# Creating and defining Deployment object
-deployment = client.ExtensionsV1beta1Deployment()
-deployment.api_version = "extensions/v1beta1"
-deployment.kind = "Deployment"
-deployment.metadata = client.V1ObjectMeta(name=deployment_name)
+# Creating and defining Job object
+job = client.V1Job()
+job.api_version = "batch/v1"
+job.kind = "Job"
+job.metadata = client.V1ObjectMeta(name=job_name)
 
-# Defining Deployment spec with Pods
-spec = client.ExtensionsV1beta1DeploymentSpec(
-    template=client.V1PodTemplateSpec())
-spec.replicas = replicas
+# Defining Job spec with Pods
+spec = client.V1JobSpec(template=client.V1PodTemplateSpec())
 spec.template.metadata = client.V1ObjectMeta(labels=pod_labels)
-deployment.spec = spec
+job.spec = spec
 
 # Defining Pod container specs
 container = client.V1Container(name=container_name)
@@ -53,10 +51,10 @@ container.image = container_image
 container.env = container_env_vars
 container.ports = [client.V1ContainerPort(container_port=container_port)]
 
-# restart_policy options: "Always"
-spec.template.spec = client.V1PodSpec(containers=[container], restart_policy="Always")
+# restart_policy options: "Always", "OnFailure", "Never"
+spec.template.spec = client.V1PodSpec(containers=[container], restart_policy="OnFailure")
 
-# Creating Deployment
-print("Creating Deployment '{0}'".format(deployment_name))
-extension.create_namespaced_deployment(namespace="default", body=deployment)
+# Creating Job
+print("Creating Job '{0}'".format(job_name))
+extension.create_namespaced_job(namespace="default", body=job)
 print("Success!")
